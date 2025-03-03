2b22e17573a716bfcdfa16434f92bc5bcdc9e5a5
An Vo <an.vo@adurolife.com>
2025-03-03 15:13:30 +0700

    feat: Add error handling and input validation for NATO alphabet conversion

--- 12NATO-alphabet/main.py
+++ 12NATO-alphabet/main.py
@@ -5,9 +5,17 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 # print(df)
 nato_dict = {row["letter"]:row["code"] for (index, row) in df.iterrows()}
 
-print(nato_dict)
+# print(nato_dict)
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ")
-phonetic_words = [nato_dict[letter.upper()] for letter in user_input]
+#----MAIN FUNCTION----
 
-print(phonetic_words)
+continue_ask = True
+while continue_ask:
+    user_input = input("Enter a word: ")
+    try:
+        phonetic_words = [nato_dict[letter.upper()] for letter in user_input]
+        print(phonetic_words)
+        continue_ask = False
+    except KeyError:
+        print("Sorry, it accept Letter only, try again!")
+        continue_ask = True
